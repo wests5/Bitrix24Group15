e87d80551be1c09349e0f775fd00495d3c6bf02f
bulentselcuk <bulentselcuk@gmail.com>
2020-04-10 13:44:14 -0500

    AttachingVideo

--- src/test/java/com/Bitrix24/tests/BulentTX_9AttachingVideo.java
+++ src/test/java/com/Bitrix24/tests/BulentTX_9AttachingVideo.java
@@ -1,4 +1,15 @@
 package com.Bitrix24.tests;
 
-public class BulentTX_9AttachingVideo {
+import com.Bitrix24.pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class BulentTX_9AttachingVideo extends TestBase{
+
+    @Test(description = " User should be able to attach to video.")
+    public void addVideo() throws InterruptedException {
+//        extentTest = extentReports.createTest("User should be able to attach to video." );
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+
+    }
 }
