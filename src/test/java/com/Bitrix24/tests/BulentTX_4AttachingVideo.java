464d8ea0144226d8257a89c6a8a66eecfe199358
bulentselcuk <bulentselcuk@gmail.com>
2020-04-11 02:43:20 -0500

    Attached Video

--- src/test/java/com/Bitrix24/tests/BulentTX_4AttachingVideo.java
+++ src/test/java/com/Bitrix24/tests/BulentTX_4AttachingVideo.java
@@ -14,7 +14,7 @@ public class BulentTX_4AttachingVideo extends TestBase{
 
     @Test(description = " User should be able to attach to video.")
     public void addVideo() throws InterruptedException {
-//        extentTest = extentReports.createTest("User should be able to attach to video." );
+       extentTest = extentReports.createTest("User should be able to attach to video." );
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
@@ -26,8 +26,12 @@ public class BulentTX_4AttachingVideo extends TestBase{
         BrowserUtilities.wait ( 3 );
         activeStreamPage.videoURL.click();
         activeStreamPage.videoURL.sendKeys ("https://www.youtube.com/watch?v=yefmcX57Eyg&t=6s");
+        wait.until(ExpectedConditions.visibilityOf(activeStreamPage.videoURL));
         BrowserUtilities.wait ( 20 );
+        wait.until(ExpectedConditions.visibilityOf(activeStreamPage.saveVideoBtn));
         activeStreamPage.saveVideoBtn.click();
+        BrowserUtilities.wait ( 5 );
+        extentTest.pass("Video has attached!");
 
 
     }
